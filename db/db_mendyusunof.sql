6d83d5e537ba7722e1e5f9f823043f5d06b5dbc7
Gero Mendy <geromendy@gmail.com>
2019-10-01 11:01:10 -0300

    Agregado route.php

---
+++ db/db_mendyusunof.sql
@@ -0,0 +1,138 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-09-2019 a las 00:47:55
+-- Versión del servidor: 10.1.32-MariaDB
+-- Versión de PHP: 7.2.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_mendyusunof`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cerveza`
+--
+
+CREATE TABLE `cerveza` (
+  `id_cerveza` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `imagen` varchar(250) NOT NULL,
+  `id_estilo` int(11) NOT NULL,
+  `amargor` int(2) NOT NULL,
+  `alcohol` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `cerveza`
+--
+
+INSERT INTO `cerveza` (`id_cerveza`, `nombre`, `imagen`, `id_estilo`, `amargor`, `alcohol`) VALUES
+(1, 'a', 'a', 3, 1, 2),
+(2, 'a', 'a', 3, 1, 2),
+(3, 'x', 'x', 4, 2, 15),
+(4, 'x', 'x', 4, 2, 15);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estilo`
+--
+
+CREATE TABLE `estilo` (
+  `id_estilo` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `color` varchar(20) NOT NULL,
+  `aroma` varchar(300) NOT NULL,
+  `apariencia` varchar(300) NOT NULL,
+  `sabor` varchar(300) NOT NULL,
+  `amargor_min` int(2) NOT NULL,
+  `amargor_max` int(2) NOT NULL,
+  `alcohol_min` int(2) NOT NULL,
+  `alcohol_max` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `estilo`
+--
+
+INSERT INTO `estilo` (`id_estilo`, `nombre`, `color`, `aroma`, `apariencia`, `sabor`, `amargor_min`, `amargor_max`, `alcohol_min`, `alcohol_max`) VALUES
+(3, 'a', 'a', 'a', 'a', 'a', 1, 1, 1, 1),
+(4, 'ba', 'b', 'b', 'b', 'b', 1, 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `mail` varchar(100) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `admin` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cerveza`
+--
+ALTER TABLE `cerveza`
+  ADD PRIMARY KEY (`id_cerveza`);
+
+--
+-- Indices de la tabla `estilo`
+--
+ALTER TABLE `estilo`
+  ADD PRIMARY KEY (`id_estilo`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cerveza`
+--
+ALTER TABLE `cerveza`
+  MODIFY `id_cerveza` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `estilo`
+--
+ALTER TABLE `estilo`
+  MODIFY `id_estilo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
