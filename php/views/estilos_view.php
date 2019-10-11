d39c6cac07b16fc918daeb1ba0b2ec39344f0101
diegousu <diegousunoff@gmail.com>
2019-10-11 19:02:37 -0300

    Cambio template para listar cervezas y agregado el estilos view

---
+++ php/views/estilos_view.php
@@ -0,0 +1,29 @@
+<?php
+
+    require_once "../libs/Smarty.class.php";
+
+    class estilos_view{
+
+        private $plantilla;
+        private $titulo;
+
+        public function __construct(){
+            $this->plantilla = new Smarty();
+            $this->titulo="Estilos";
+        }
+
+        public function generateTable($list_estilos){
+            $data = array();
+
+            foreach ($list_estilos as $estilo) {
+                array_push($data,$estilo);
+            }
+            $this->plantilla->assign('titulo',$this->titulo);
+            $this->plantilla->assign('estilos',$data);
+            $this->plantilla->display("../templates/estilos_table.tpl");
+        }
+
+
+    }
+
+?>
