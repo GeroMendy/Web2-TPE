a859d254c81d716b91ff017810d2801bdb42ec84
diegousu <diegousunoff@gmail.com>
2019-11-28 17:36:37 -0300

    diego easter

--- js/comentarios_cerveza.js
+++ js/comentarios_cerveza.js
@@ -2,18 +2,20 @@
 
 document.addEventListener("DOMContentLoaded",function(){
 
-    console.log("Comentarios_cerveza.js Working...");
+    let intervalEliminar;
 
     let form_comentario = document.querySelector("#commentform");
     if(form_comentario !=null){
         form_comentario.addEventListener("submit",postComentario);
     }
 
+    document.querySelector("#agregar_comentario_texto").addEventListener("click",easterEgg);
+
     let comentarios_vue = new Vue({
         el: "#vue-comentarios",
         data:{
+            id_usuario_logged: '',
             comentarios: [],
-            usuario_logged: 0,
             adminLogged: 0,
             promedio: 0
         }
@@ -36,9 +38,8 @@ document.addEventListener("DOMContentLoaded",function(){
         };
         fetch(getUrlAddComentario(),paquete_post)
         .then(p=>{
-            console.log("Comentario Agregado Correctamente.");
-            getComentarios();
-        })
+            esperarParaComentarios();
+            })
         .catch(error=>console.log(error));
     }
 
@@ -55,16 +56,16 @@ document.addEventListener("DOMContentLoaded",function(){
             if (comentarios.length!=0)
                 promedio = (valoracionTotal/comentarios.length);
             else promedio =0;
-            document.querySelector("#js_valoracion").innerHTML="Valoración promedio: "+(Math.round(promedio*100)/100+" / 5");
+            document.querySelector(".js_valoracion").innerHTML="Valoración promedio: "+Math.round(promedio);
             if (isAdmin())
-                setTimeout(funtion =>{linkearBotonesEliminarComentario(comentarios)},200);//Espero a que se generen los botones eliminar.
+                intervalEliminar = setTimeout(funtion =>{linkearAnchorsEliminarComentario(comentarios)},200);//genero un bucle porque los button no estan generados.
         })
         .catch(error => console.log(error));
         comentarios_vue.adminLogged = isAdmin();
-        comentarios_vue.id_usuario_logged = getLogged();
+        comentarios_vue.id_usuario_logged = getIdLogged();
     }
 
-    function linkearBotonesEliminarComentario(com){
+    function linkearAnchorsEliminarComentario(com){
         let anchors_eliminar=document.querySelectorAll(".js_eliminar_comentario");
         while (anchors_eliminar.length!=com.length){
             anchors_eliminar=document.querySelectorAll(".js_eliminar_comentario");
@@ -72,44 +73,51 @@ document.addEventListener("DOMContentLoaded",function(){
         for (let i = 0; i< anchors_eliminar.length; i++){
             anchors_eliminar[i].addEventListener("click",function(){
                 eliminarComentario(com[i].id_comentario);
-            },{once:true});
+            });
         }
     }
 
-    function eliminarComentario(id_com){    
+    async function eliminarComentario(id_com){
         console.log("Eliminando comentario "+id_com);
-        let urlBorrar=getUrlEliminar(id_com);
-        let paquete_delete = {
-            "method":"DELETE",
-        };
-
+        let urlBorrar="api/comentario/eliminar/"+id_com;
         let tit_anterior=document.title;
         document.title="Borrando...";
         try{
-            fetch(urlBorrar,paquete_delete)
-            .then(r=>{
-                console.log("Eliminado Comentario "+id_com);
+            let prom=await fetch(urlBorrar,{"method":"delete"})
+            if (prom.ok){
                 getComentarios();
-            });
+                console.log("Borrado");
+            }
         }
         catch(error){console.log(error);}
         document.title=tit_anterior;
     }
     
+    async function borrar(id_delete){
+        let urlborrar=url+"/"+id_delete;
+        document.title="Borrando...";
+        try{let prom=await fetch(urlborrar,{"method":"delete"})
+        }
+        catch(error){console.log(error);}
+        document.title="Catalogo de Cervezas";
+        mostrartabla();
+    }
+
+
     function isAdmin(){
-        let input = document.querySelector("#js_admin");
+        let input = document.querySelector("#isAdmin");
         if(input!=null){
             return input.value;
         }else{
             return false;
         }
     }
-    function getLogged(){
-        let input = document.querySelector("#js_logged");
+    function getIdLogged(){
+        let input = document.querySelector("#id_logged");
         if(input!=null){
             return input.value;
         }else{
-            return false;
+            return -1;
         }
     }
 
@@ -118,30 +126,45 @@ document.addEventListener("DOMContentLoaded",function(){
         let newPath = "api/comentario";
         return replaceUrl(replaced,newPath);
     }
-    function getUrlEliminar(id_com){
-        let deleted = "cerveza";
-        let newPath = "api/comentario/eliminar/";
-        let url = deleteSubtringURL(deleted);
-
-        return url + newPath + id_com;
+    function getUrlEliminar(){
+        let replaced = "cerveza";
+        let newPath = "api/comentario/eliminar";
+        return replaceUrl(replaced,newPath);
     }
     function getUrlAddComentario(){
         let replaced = "cerveza";
         let newPath = "api/comentario/agregar";
         return replaceUrl(replaced,newPath);
     }
-    function deleteSubtringURL(deleted){    //Busca la ultima coincidencia y elimina todo desde ahi.
-        let url = window.location.pathname;
-        let position = url.lastIndexOf(deleted);
-        return url.substring(0,position);
-    }
-    function replaceUrl(replaced,newPath){  //Busca la ultima coincidencia y la reemplaza, manteniendo el substring posterior.
+    function replaceUrl(replaced,newPath){
         let url = window.location.pathname;
         let inicio = url.lastIndexOf(replaced);
         let fin = inicio + replaced.length;
         url = url.substring(0,inicio) + newPath + url.substring(fin,url.length);
         return url;
     }
-    
+    function esperarParaComentarios(){
+        setTimeout(getComentarios(),300);
+    }
     getComentarios();
+    
+    let activado=false;
+    function easterEgg(){
+        let text=document.querySelector("#agregar_comentario_texto").value;
+        if (text==="juan codigo"){
+            if (!activado){
+                let imgs=document.getElementsByTagName("img");
+                for (let i =0; i<imgs.length;i++){
+                    let source= imgs[i].src;
+                    imgs[i].src=source.substring(0,source.lastIndexOf("cervezas"))+"juancodigo.jpg";
+                }
+                activado=true;
+            }
+            let html=document.getElementsByTagName("body");
+            hackerman(html[0]);
+        }
+    }
+    function hackerman(html){
+        html.classList.toggle("hackerman");
+    }
 });
